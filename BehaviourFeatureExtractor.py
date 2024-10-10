400e6249390c019f40f3be8ad0d0f50e0c1fe6d3
danaebroustail <danaebroustail@gmail.com>
2024-10-10 15:19:54 +0200

    added pup_speed computation and included column names as input parameters in bhv_extraction utility functions

--- BehaviourFeatureExtractor.py
+++ BehaviourFeatureExtractor.py
@@ -19,29 +19,37 @@ def convert_seconds_to_frame(seconds, frame_rate = 30):
 # - the following utility functions assume that the input is a processed DLC file.
 
 
-def compute_speed(df_DLC):
+def compute_speed(df_DLC, x_col, y_col, speed_col, time_col = 'time_seconds'):
 
     # compute speed
-    distance = np.sqrt(np.diff(df_DLC['msTop_x'])**2 + np.diff(df_DLC['msTop_y'])**2)
-    time = np.diff(df_DLC['time_seconds'])
+    distance = np.sqrt(np.diff(df_DLC[x_col])**2 + np.diff(df_DLC[y_col])**2)
+    time = np.diff(df_DLC[time_col])
     speed = distance/time
     # add speed to the dataframe
-    df_DLC['speed_cm/s'] = np.append(speed, 0)
+    df_DLC[speed_col] = np.append(speed, 0)
     
     return df_DLC
 
-def compute_distance_to_pup(df_DLC):
+def compute_distance_to_pup(df_DLC,
+                            msTop_x_col = 'msTop_x', msTop_y_col = 'msTop_y',
+                            pup_x_col = 'pup_x', pup_y_col = 'pup_y',
+                            distance_col = 'distance_to_pup'):
     # compute distance to pup
 
-    distance = np.sqrt((df_DLC['msTop_x'] - df_DLC['pup_x'])**2 + (df_DLC['msTop_y'] - df_DLC['pup_y'])**2)
-    df_DLC['distance_to_pup'] = distance 
+    distance = np.sqrt((df_DLC[msTop_x_col] - df_DLC[pup_x_col])**2 + (df_DLC[msTop_y_col] - df_DLC[pup_y_col])**2)
+    df_DLC[distance_col] = distance 
     return df_DLC
 
-def compute_head_angle_to_pup(df_DLC, add_vector_columns = False):
+def compute_head_angle_to_pup(df_DLC, add_vector_columns = False,
+                              earRight_x_col = 'earRight_x', earRight_y_col = 'earRight_y',
+                              earLeft_x_col = 'earLeft_x', earLeft_y_col = 'earLeft_y',
+                              nose_x_col = 'nose_x', nose_y_col = 'nose_y',
+                              pup_x_col = 'pup_x', pup_y_col = 'pup_y',
+                              head_angle_to_pup_col = 'head_angle_to_pup_degrees'):
 
     # define mouse head direction with respect to average of ears and nose
-    earRight_x, earRight_y, earLeft_x, earLeft_y,  = df_DLC['earRight_x'], df_DLC['earRight_y'], df_DLC['earLeft_x'], df_DLC['earLeft_y']
-    nose_x, nose_y, pup_x, pup_y = df_DLC['nose_x'], df_DLC['nose_y'], df_DLC['pup_x'], df_DLC['pup_y']
+    earRight_x, earRight_y, earLeft_x, earLeft_y,  = df_DLC[earRight_x_col], df_DLC[earRight_y_col], df_DLC[earLeft_x_col], df_DLC[earLeft_y_col]
+    nose_x, nose_y, pup_x, pup_y = df_DLC[nose_x_col], df_DLC[nose_y_col], df_DLC[pup_x_col], df_DLC[pup_y_col]
 
     # compute between ears coordinate
     between_ears_x, between_ears_y = (earRight_x + earLeft_x)/2, (earRight_y + earLeft_y)/2
@@ -75,11 +83,26 @@ def compute_head_angle_to_pup(df_DLC, add_vector_columns = False):
     angle = np.arccos(cos_theta)
 
     # add angle to the dataframe (as degrees)
-    df_DLC['head_angle_to_pup_degrees'] = angle * 180/np.pi
+    df_DLC[head_angle_to_pup_col] = angle * 180/np.pi
     
     return df_DLC
 
-def extract_base_parameters(df_DLC, df_summary):
+def extract_base_parameters(df_DLC, df_summary,
+                            trial_end_col = 'BehavRecdTrialEndSecs',
+                            pup_drop_col = 'PupDispDropSecs',
+                            trial_num_col = 'TrialNum',
+                            frame_index_col = 'frame_index',
+                            pup_speed_col = 'pup_speed_px/s',
+                            mouse_speed_col = 'ms_speed_px/s',
+                            distance_col_mouse_pup = 'distance_mouse_to_pup',
+                            distance_col_head_pup = 'distance_head_to_pup',
+                            earRight_x_col = 'earRight_x', earRight_y_col = 'earRight_y',
+                            earLeft_x_col = 'earLeft_x', earLeft_y_col = 'earLeft_y',
+                            head_x_col = 'endHeadbar_x', head_y_col = 'endHeadbar_y',
+                            nose_x_col = 'nose_x', nose_y_col = 'nose_y',
+                            pup_x_col = 'pup_x', pup_y_col = 'pup_y',
+                            msTop_x_col = 'msTop_x', msTop_y_col = 'msTop_y',
+                            head_angle_to_pup_col = 'head_angle_to_pup_degrees'):
     """
     Extracts base parameters such as speed, distance to pup, and head angle to pup for each trial 
     from the given DataFrame.
@@ -94,12 +117,14 @@ def extract_base_parameters(df_DLC, df_summary):
     """
 
     # for each trial, get the start and end frames
-    end_times, start_times, trial_nums = df_summary['BehavRecdTrialEndSecs'], df_summary['PupDispDropSecs'], df_summary['TrialNum']
+    end_times, start_times, trial_nums = df_summary[trial_end_col], df_summary[pup_drop_col], df_summary[trial_num_col]
 
     # create NaN columns for speed, distance to pup and head angle to pup
-    df_DLC['speed_cm/s'] = np.nan
-    df_DLC['distance_to_pup'] = np.nan
-    df_DLC['head_angle_to_pup_degrees'] = np.nan
+    df_DLC[mouse_speed_col] = np.nan
+    df_DLC[pup_speed_col] = np.nan
+    df_DLC[distance_col_head_pup] = np.nan
+    df_DLC[distance_col_mouse_pup] = np.nan
+    df_DLC[head_angle_to_pup_col] = np.nan
 
     # iterate over each trial
     for end, start, trial_num in zip(end_times, start_times, trial_nums):
@@ -109,15 +134,29 @@ def extract_base_parameters(df_DLC, df_summary):
             print(f"Processing trial {trial_num} Start frame: {start_frame} End frame: {end_frame}")
 
             # get the data for the trial
-            mask = (df_DLC['frame_index'] >= start_frame) & (df_DLC['frame_index'] <= end_frame)
+            mask = (df_DLC[frame_index_col] >= start_frame) & (df_DLC[frame_index_col] <= end_frame)
             trial_DLC = df_DLC.loc[mask, :] 
 
             # compute speed
-            trial_DLC = compute_speed(trial_DLC)
+            trial_DLC = compute_speed(trial_DLC, x_col = msTop_x_col, y_col = msTop_y_col, speed_col = mouse_speed_col)
+            trial_DLC = compute_speed(trial_DLC, x_col = pup_x_col, y_col = pup_y_col, speed_col = pup_speed_col)
+
             # compute distance to pup
-            trial_DLC = compute_distance_to_pup(trial_DLC)
+            trial_DLC = compute_distance_to_pup(trial_DLC, x_col = msTop_x_col, y_col = msTop_x_col,
+                                                pup_x_col = pup_x_col, pup_y_col = pup_y_col,
+                                                distance_col = distance_col_mouse_pup)
+            
+            trial_DLC = compute_distance_to_pup(trial_DLC, x_col = head_x_col, y_col = head_y_col,
+                                                pup_x_col = pup_x_col, pup_y_col = pup_y_col,
+                                                distance_col = distance_col_head_pup)
+            
             # compute head angle to pup
-            trial_DLC = compute_head_angle_to_pup(trial_DLC)
+            trial_DLC = compute_head_angle_to_pup(trial_DLC, add_vector_columns = False,
+                                                  earRight_x_col = earRight_x_col, earRight_y_col = earRight_y_col,
+                                                  earLeft_x_col = earLeft_x_col, earLeft_y_col = earLeft_y_col,
+                                                  nose_x_col = nose_x_col, nose_y_col = nose_y_col,
+                                                  pup_x_col = pup_x_col, pup_y_col = pup_y_col,
+                                                  head_angle_to_pup_col = head_angle_to_pup_col)
             
             # update the dataframe
             df_DLC.loc[mask, :] = trial_DLC
@@ -131,37 +170,51 @@ def extract_base_parameters(df_DLC, df_summary):
 def plot_mouse_angle_to_pup(trial_df_DLC,
                             ylim = None,
                             xlim = None,
-                            frame_index = None):
+                            frame_index_number = None,
+                            frame_index_col = 'frame_index',
+                            msTop_x_col = 'msTop_x',
+                            msTop_y_col = 'msTop_y',
+                            earRight_x_col = 'earRight_x',
+                            earRight_y_col = 'earRight_y',
+                            earLeft_x_col = 'earLeft_x',
+                            earLeft_y_col = 'earLeft_y',
+                            nose_x_col = 'nose_x',
+                            nose_y_col = 'nose_y',
+                            pup_x_col = 'pup_x',
+                            pup_y_col = 'pup_y',
+                            between_ears_x_col = 'between_ears_x',
+                            between_ears_y_col = 'between_ears_y',
+                            head_angle_to_pup = 'head_angle_to_pup_degrees'):
 
     # select a random frame in the trial
-    if frame_index is None:
-        frame_index_min, frame_index_max = trial_df_DLC['frame_index'].min(), trial_df_DLC['frame_index'].max()
-        frame_index = random.randint(frame_index_min, frame_index_max)
-    trial_1_DLC_frame = trial_df_DLC.loc[trial_df_DLC['frame_index'] == frame_index, :]
+    if frame_index_number is None:
+        frame_index_min, frame_index_max = trial_df_DLC[frame_index_col].min(), trial_df_DLC[frame_index_col].max()
+        frame_index_number = random.randint(frame_index_min, frame_index_max)
+    trial_1_DLC_frame = trial_df_DLC.loc[trial_df_DLC[frame_index_col] == frame_index_number, :]
 
     # plot the frame
     fig, ax = plt.subplots(1, 1, figsize=(6, 6))
 
     if xlim is None or ylim is None:
-        xlim, ylim =  max(trial_df_DLC['msTop_x'].max(), trial_df_DLC['pup_x'].max()), max(trial_df_DLC['msTop_y'].max(), trial_df_DLC['pup_y'].max())
+        xlim, ylim =  max(trial_df_DLC[msTop_x_col].max(), trial_df_DLC[pup_x_col].max()), max(trial_df_DLC[msTop_y_col].max(), trial_df_DLC[pup_y_col].max())
 
-    trial_1_DLC_frame.plot(x='earRight_x', y='earRight_y', style='o', ax=ax, xlim=(0, xlim), ylim=(0, ylim), color = 'black')
-    trial_1_DLC_frame.plot(x='earLeft_x', y='earLeft_y', style='o', ax=ax, xlim=(0, xlim), ylim=(0, ylim), color = 'black')
-    trial_1_DLC_frame.plot(x='nose_x', y='nose_y', style='o', ax=ax, xlim=(0, xlim), ylim=(0, ylim), color = 'red')
-    trial_1_DLC_frame.plot(x='pup_x', y='pup_y', style='o', ax=ax,xlim=(0, xlim), ylim=(0, ylim), color = 'purple')
-    trial_1_DLC_frame.plot(x='between_ears_x', y='between_ears_y', style='o', ax=ax, xlim=(0, xlim), ylim=(0, ylim), color = 'blue')
+    trial_1_DLC_frame.plot(x=earRight_x_col, y=earRight_y_col, style='o', ax=ax, xlim=(0, xlim), ylim=(0, ylim), color = 'black')
+    trial_1_DLC_frame.plot(x=earLeft_x_col, y=earLeft_y_col, style='o', ax=ax, xlim=(0, xlim), ylim=(0, ylim), color = 'black')
+    trial_1_DLC_frame.plot(x=nose_x_col, y=nose_y_col, style='o', ax=ax, xlim=(0, xlim), ylim=(0, ylim), color = 'red')
+    trial_1_DLC_frame.plot(x=pup_x_col, y=pup_y_col, style='o', ax=ax,xlim=(0, xlim), ylim=(0, ylim), color = 'purple')
+    trial_1_DLC_frame.plot(x=between_ears_x_col, y=between_ears_y_col, style='o', ax=ax, xlim=(0, xlim), ylim=(0, ylim), color = 'blue')
 
     # draw lines
     # draw line from nose to between ears in blue
-    ax.plot([trial_1_DLC_frame['nose_x'].values[0], trial_1_DLC_frame['between_ears_x'].values[0]],
-            [trial_1_DLC_frame['nose_y'].values[0], trial_1_DLC_frame['between_ears_y'].values[0]], 'k-', color = 'blue')
+    ax.plot([trial_1_DLC_frame[nose_x_col].values[0], trial_1_DLC_frame[between_ears_x_col].values[0]],
+            [trial_1_DLC_frame[nose_y_col].values[0], trial_1_DLC_frame[between_ears_y_col].values[0]], 'k-', color = 'blue')
 
     # draw line from between ears to pup in green
-    ax.plot([trial_1_DLC_frame['between_ears_x'].values[0], trial_1_DLC_frame['pup_x'].values[0]],
-            [trial_1_DLC_frame['between_ears_y'].values[0], trial_1_DLC_frame['pup_y'].values[0]], 'k-', color = 'green')
+    ax.plot([trial_1_DLC_frame[between_ears_x_col].values[0], trial_1_DLC_frame[pup_x_col].values[0]],
+            [trial_1_DLC_frame[between_ears_y_col].values[0], trial_1_DLC_frame[pup_y_col].values[0]], 'k-', color = 'green')
 
     # add angle value to the plot in the title
-    angle = trial_1_DLC_frame['head_angle_to_pup_degrees'].values[0]
+    angle = trial_1_DLC_frame[head_angle_to_pup].values[0]
 
     ax.set_title("Angle between mouse head direction and pup direction: " + str(angle) + " degrees")
     ax.set_xlabel("X")
